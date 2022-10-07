1f3691288860958cb11efe10d3bd78d3d16296ce
SAHIL0455 <sahilpawar1004@gmail.com>
2022-10-07 19:56:36 +0530

    ADDED: ability to create the contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,19 @@
 console.log("Welcome to Address Book");
+class Contact {
+    constructor(...params) {
+      this.firstName = params[0];
+      this.lastName = params[1];
+      this.address = params[2];
+      this.city = params[3];
+      this.state = params[4];
+      this.zip = params[5];
+      this.phoneNumber = params[6];
+      this.email = params[7];
+    }
+    toString() {
+      return ("First Name : " +this.firstName +", Last Name : " +this.lastName +", Address : " +this.address +", City : " +this.city +", State : " +this.state +", Zip : " +this.zip + ", Phone Number : " +this.phoneNumber +", Email : " +this.email);
+    }
+  }
+  
+  let contact = new Contact("Sahil","Pawar","Chiplun","Ratnagiri","MH",415601,7350310455,"sahilpawar1004@gmail.com");
+  console.log(contact.toString());
